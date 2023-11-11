a710e517fb095ede9e5a9737ac330ed9eb0e3016
cezarazvdo <cezardiazevedo@gmail.com>
2023-11-11 11:38:53 -0300

    feat: adicionado o construtor com data

--- NewTalents/Calculadora.cs
+++ NewTalents/Calculadora.cs
@@ -8,35 +8,43 @@ namespace NewTalents
 {
     public class Calculadora
     {
-        private List<string> historico = new List<string>();
+        private List<string> historico;
+        private DateTime data = new DateTime();
+        public Calculadora(DateTime data)
+        {
+            historico = new List<string>();
+            this.data = data;
+        }
 
         public int Somar(int val1, int val2)
         {
             var result = val1 + val2;
-            historico.Add($"Soma: {val1} + {val2} = {result}");
+            historico.Insert(0, $"Soma: {val1} + {val2} = {result} - data: {data}");
             return result;
         }
         public int Subtrair(int val1, int val2)
         {
             var result = val1 - val2;
-            historico.Add($"Subtrair: {val1} - {val2} = {result}");
+            historico.Insert(0, $"Subtrair: {val1} - {val2} = {result} - data: {data}");
             return result;
         }
         public int Multiplicar(int val1, int val2)
         {
             var result = val1 * val2;
-            historico.Add($"Multiplicar: {val1} * {val2} = {result}");
+            historico.Insert(0, $"Multiplicar: {val1} * {val2} = {result} - data: {data}");
             return result;
         }
         public int Dividir(int val1, int val2)
         {
             var result = val1 / val2;
-            historico.Add($"Dividir: {val1} / {val2} = {result}");
+            historico.Insert(0, $"Dividir: {val1} / {val2} = {result} - data: {data}");
             return result;
         }
 
         public List<string> Historico()
         {
+            historico.RemoveRange(3, historico.Count - 3);
+
             return historico;
         }
     }
--- TestsNewTalents/UnitTest1.cs
+++ TestsNewTalents/UnitTest1.cs
@@ -4,13 +4,13 @@ namespace TestsNewTalents
 {
     public class UnitTest1
     {
+        Calculadora calc = new Calculadora(DateTime.Now);
+
         [Theory]
         [InlineData(1, 2, 3)]
         [InlineData(4, 5, 9)]
         public void TesteSomar(int val1, int val2, int resultado)
         {
-            Calculadora calc = new Calculadora();
-
             var resultadoCalc = calc.Somar(val1, val2);
 
             Assert.Equal(resultado, resultadoCalc);
@@ -21,8 +21,6 @@ namespace TestsNewTalents
         [InlineData(5, 5, 0)]
         public void TesteSubtrair(int val1, int val2, int resultado)
         {
-            Calculadora calc = new Calculadora();
-
             var resultadoCalc = calc.Subtrair(val1, val2);
 
             Assert.Equal(resultado, resultadoCalc);
@@ -33,8 +31,6 @@ namespace TestsNewTalents
         [InlineData(4, 5, 20)]
         public void TesteMultiplicar(int val1, int val2, int resultado)
         {
-            Calculadora calc = new Calculadora();
-
             var resultadoCalc = calc.Multiplicar(val1, val2);
 
             Assert.Equal(resultado, resultadoCalc);
@@ -45,8 +41,6 @@ namespace TestsNewTalents
         [InlineData(5, 5, 1)]
         public void TesteDividir(int val1, int val2, int resultado)
         {
-            Calculadora calc = new Calculadora();
-
             var resultadoCalc = calc.Dividir(val1, val2);
 
             Assert.Equal(resultado, resultadoCalc);
@@ -54,16 +48,12 @@ namespace TestsNewTalents
 
         public void TestarDivisaoPorZero()
         {
-            Calculadora calc = new Calculadora();
-
             Assert.Throws<DivideByZeroException>(() => calc.Dividir(3, 0));
         }
 
         [Fact]
         public void TestarHistorico()
         {
-            Calculadora calc = new Calculadora();
-
             calc.Somar(1, 2);
             calc.Somar(3, 4);
             calc.Somar(5, 6);
@@ -71,7 +61,7 @@ namespace TestsNewTalents
             var lista = calc.Historico();
 
             Assert.NotEmpty(calc.Historico());
-            Assert.Equal(4, lista.Count());
+            Assert.Equal(3, lista.Count());
         }
     }
 }
